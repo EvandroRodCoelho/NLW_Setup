6f719859aa2326aaf7b7de35107807949267b1b4
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-01-16 22:02:08 -0300

    cors

--- serve/src/serve.ts
+++ serve/src/serve.ts
@@ -1,11 +1,12 @@
 import Fastify from 'fastify';
-import  {PrismaClient}  from '@prisma/client';
-
+import  { PrismaClient }  from '@prisma/client';
+import cors from '@fastify/cors';
 const app = Fastify();
 const prisma = new PrismaClient();
 
+app.register(cors);
+
 app.get('/', async () => {
-  console.log('foi');
   const habits = await prisma.habits.findMany({});
   return habits;
 });
