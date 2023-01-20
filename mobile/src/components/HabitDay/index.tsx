e11da38a6f2133d0434c72ed5a7d16613f64b295
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-01-19 22:04:08 -0300

    Components

--- mobile/src/components/HabitDay/index.tsx
+++ mobile/src/components/HabitDay/index.tsx
@@ -1,19 +1,23 @@
 
-import { TouchableOpacity, Dimensions } from "react-native";
+import { TouchableOpacity, Dimensions, TouchableOpacityProps } from "react-native";
+
+interface Props extends TouchableOpacityProps{}
+
 const weekDays = 7;
 const screenHorizontalPadding = (32 * 2) / 5;
 export const dayMarginBetween = 8;
-export const daySize = (Dimensions.get('screen').width / weekDays)- (screenHorizontalPadding * 5);
-export function HabitDay() {
+export const daySize = (Dimensions.get('screen').width / weekDays)- (screenHorizontalPadding + 5);
+export function HabitDay({...rest}:Props) {
 
   return (
     <TouchableOpacity
-    className="bg-zinc-900 rounded-lg border-2 m1 border-zinc-800"
+    className="bg-zinc-900 rounded-lg  border-2 m-1 border-zinc-800"
       style={{
         width: daySize,
         height:daySize
       }}
       activeOpacity={0.7}
+      {...rest}
     />
   );
 }
--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -2,16 +2,18 @@ import { View, TouchableOpacity,Text } from 'react-native';
 import Logo from '../../assets/logo.svg';
 import { Feather } from '@expo/vector-icons';
 import colors from 'tailwindcss/colors';
-
+import { useNavigation } from '@react-navigation/native';
 export function Header() {
-
+  const { navigate } = useNavigation();
 
   return (
     <View className='w-full flex-row items-center justify-between'>
       <Logo />
       <TouchableOpacity
         className='flex-row h-11 px-4 border border-violet-500 rounded-lg items-center'
-        activeOpacity={0.7}>
+        activeOpacity={0.7}
+        onPress={()=> navigate('new')}
+      >
         <Feather
           name='plus'
           color={colors.violet[500]}
